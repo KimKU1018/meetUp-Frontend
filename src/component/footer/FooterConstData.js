783491d53fc0571114f98e09b5753d75f79b4e71
Dev_KimKU <86593582+KimKU1018@users.noreply.github.com>
2022-11-16 15:40:02 +0900

    [Feat] Create FooterConstData.js

---
+++ src/component/footer/FooterConstData.js
@@ -0,0 +1,92 @@
+
+export const WEBSITE_TERMS_MENU = [
+  {
+    id: "1",
+    termsElement: "개인정보처리방침",
+  },
+  {
+    id: "2",
+    termsElement: "쿠키 정책",
+  },
+
+  {
+    id: "3",
+    termsElement: "쿠키 설정",
+  },
+  {
+    id: "4",
+    termsElement: "웹사이트 이용약관",
+  },
+  { id: "5", termsElement: "Responsible disclosure" },
+];
+
+export const COMPANY_INFO = [
+  { id: "1", companyInfo: "4GATE" },
+  {
+    id: "2",
+    companyInfo: "주소 : 대한민국 서울특별시 ㅇㅇㅇ ㅇㅇㅇ ㅇㅇㅇ",
+  },
+  {
+    id: "3",
+    companyInfo: "PM: 김경욱",
+  },
+  {
+    id: "4",
+    companyInfo: "기획팀: 김희연, 권영은, 양예은",
+  },
+  {
+    id: "5",
+    companyInfo: "디자이너: 최성경",
+  },
+  {
+    id: "6",
+    companyInfo: "개발팀: 고승원, 김경욱, 이정진, 안정후",
+  },
+];
+
+export const MEMBERSHIP_SIGNUP_GUIDING_DATA = [
+  {
+    title: "IKEA Family",
+    discription:
+      "지금 IKEA Family에 무료로 가입하고 다양한 멤버 전용 혜택을 누리세요.",
+    buttonText: "IKEA Family 가입하기",
+  },
+  {
+    title: "IKEA Business Network",
+    discription:
+      "여러분의 더 나은 비즈니스 환경을 위한 다양한 혜택들을 받으세요",
+    buttonText: "IKEA Business Network 가입하기",
+  },
+];
+
+export const FOOTER_ICON_DATA = [
+  {
+    id: "1",
+    icon: "mail",
+  },
+  {
+    id: "2",
+    icon: "call",
+  },
+  {
+    id: "3",
+    icon: "home",
+  },
+  {
+    id: "4",
+    icon: "settings",
+  },
+];
+
+export const LANGUAGE_OPTION_CHANGE_BOX = [
+  {
+    id: "1",
+    value: "korea-option",
+    language: "한국어",
+  },
+  {
+    id: "2",
+    value: "english-option",
+    language: "english",
+  },
+];
