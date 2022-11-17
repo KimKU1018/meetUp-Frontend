f8610365987cd9db2e5c8b7d0e17dba1d9c3d068
Dev_KimKU <86593582+KimKU1018@users.noreply.github.com>
2022-11-17 20:45:11 +0900

    Create MENUS.js

---
+++ src/component/nav/MENUS.js
@@ -0,0 +1,7 @@
+const MENUS = [
+    { id: 1, title: "이력서 게시판", type: "big-text" },
+    { id: 2, title: "취준 플러스", type: "big-text" },
+    { id: 3, title: "홈", type: "big-text" },
+  ];
+  
+  export default MENUS;
