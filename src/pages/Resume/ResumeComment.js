951f71cd97ded1525928aa9815457f74ccf37c4b
Dev_KimKU <86593582+KimKU1018@users.noreply.github.com>
2022-11-23 10:49:17 +0900

    Update ResumeComment.js

--- src/pages/Resume/ResumeComment.js
+++ src/pages/Resume/ResumeComment.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import ResumeButton from './ResumeButton';
+import DeleteButton from './DeleteButton';
 import API from '../../../config';
 
 const DetailComment = ({
